77c7a0856972ee8fd1d65f7cee5778c04ab75fd8
Mohammad Ahmadi <iqbal_dyncorp@live.com>
2023-04-20 16:24:27 -0700

    small cups is functioning now

--- script.js
+++ script.js
@@ -1,1 +1,27 @@
+const smallCups = document.querySelectorAll('.cup-small')
+const liters = document.getElementById('liters')
+const percentage = document.getElementById('percentage')
+const remained = document.getElementById('remained')
 
+// loop through small cups
+smallCups.forEach((cup, idx) => {
+  cup.addEventListener('click', () => {
+    highlightCups(idx)
+  })
+})
+
+function highlightCups(idx) {
+  if (
+    smallCups[idx].classList.contains('full') &&
+    !smallCups[idx].nextElementSibling.classList.contains('full')
+  ) {
+    idx--
+  }
+  smallCups.forEach((cup, idx2) => {
+    if (idx2 <= idx) {
+      cup.classList.add('full')
+    } else {
+      cup.classList.remove('full')
+    }
+  })
+}
